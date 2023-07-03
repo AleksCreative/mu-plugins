e9b19f5aa9da5624b3f2b02496f9c42e577fd821
AleksCreative <aleksdesign@yahoo.co.uk>
2023-07-03 20:57:33 +0100

    rewrite slug to events

--- university-post-types.php
+++ university-post-types.php
@@ -1,5 +1,6 @@
 <?php function university_post_types() {
     register_post_type('event', array(
+        'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
         'menu_icon' => 'dashicons-calendar',
